2d5501ead87201cabbed3582b140ba3892f5c1cb
Anushka Bhojne <anushka.m.bhojne@gmail.com>
2025-10-08 17:17:15 +1100

    Create init.py

---
+++ engine/extensible-framework-plugin-architecture/security/init.py
@@ -0,0 +1,10 @@
+"""
+Security Package
+
+Provides security-related modules, including credential management,
+encryption, and access control.
+
+Author: Senior Lead, AutoAudit
+"""
+
+#Package initialisation
